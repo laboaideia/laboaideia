ba8dfb21ba61076cf6a1d60276a17d19a5ede036
Jurandy Soares <jurandysoares@users.noreply.github.com>
2023-02-17 20:16:05 -0300

    fix: instala-pacotes-winget.ps1

--- Scripts/instala-pacotes-winget.ps1
+++ Scripts/instala-pacotes-winget.ps1
@@ -1,9 +1,16 @@
-if ($args.Length -eq 2) {
-   if (Test-Path -Path $args[1] -PathType Leaf) {
-      Import-Csv -Path $args[1] -Header Pacote | ForEach-Object {
-         winget install --id $_.Pacote
+if ($args.Length -eq 1) {
+   $arq_pacotes = $args[0]
+   if (Test-Path -Path $arq_pacotes -PathType Leaf) {
+      Import-Csv -Path $arq_pacotes -Header Pacote | ForEach-Object {
+         $pacote = $_.Pacote
+         Write-Host "Instalando pacote $pacote ..."
+         winget install --id $pacote
+         if ($LastExitCode) {
+            Write-Host "O pacote $pacote foi instalado com sucesso."
+         }
       }
    }
 }
+
 
 # Exemplo: .\instala-pacotes-winget.ps1 pacotes.txt
