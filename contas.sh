b42fed2a47ed9ea5d61d387f3cacb41c1f1f7b68
Jurandy Soares <jurandysoares@users.noreply.github.com>
2023-08-07 10:06:40 -0300

    Update contas.sh

--- contas.sh
+++ contas.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Cria uma conta para cada uma das turmas do campus
+contas=($(echo info{1..4}{m,v} meca{1..4}{m,n,v} redes{1..4} tsi{1..6}))
 
-for usuario in info{1..4}{m,v} meca{1..4}{m,n,v} redes{1..4} tsi{1..4}
+for usuario in ${contas[@]}
 do
   echo "Criação de conta para ${usuario^}"
   sudo useradd -m -s /bin/bash -c "${usuario^}" $usuario
