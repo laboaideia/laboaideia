095d81746b530047513d985d7233cb5c40089986
Jurandy Soares <jurandysoares@gmail.com>
2023-08-23 14:38:31 -0300

    feat: <contas.sh> Mudança de shell para zsh

--- contas.sh
+++ contas.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Cria uma conta para cada uma das turmas do campus
 contas=($(echo info{1..4}{m,v} meca{1..4}{m,n,v} redes{1..4} tsi{1..6}))
@@ -6,11 +6,11 @@ contas=($(echo info{1..4}{m,v} meca{1..4}{m,n,v} redes{1..4} tsi{1..6}))
 for usuario in ${contas[@]}
 do
   echo "Criação de conta para ${usuario^}"
-  sudo useradd -m -s /bin/bash -c "${usuario^}" $usuario
+  sudo useradd -m -s /bin/zsh -c "${(C)usuario}" $usuario
   
   echo "Troca da senha para ${usuario}"
   echo "${usuario}:${usuario}" | sudo chpasswd
   
   echo "Forçando a troca de senha na primeira sessão."
-  sudo chage -d 0 ${usuario}  
+  sudo chage -d 0 ${usuario}
 done
